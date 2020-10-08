a8f165e935a84c62a47fe6361f66a80ee31dc324
Shannon Makenna <shannonmakenna@chartyn.com>
2020-10-08 13:40:38 -0400

    working out spotify auth

--- server/index.js
+++ server/index.js
@@ -1,52 +1,52 @@
+require('dotenv').config();
 const express = require('express')
 const app = express()
-const cookieSession = require('cookie-session')
-const passport = require('passport')
 const cors = require('cors')
+const querystring = require('querystring')
 const isLoggedIn=require('./auth')
-require('./passport')
-const PORT = 8888
+const axios =require('axios');
+const fetch = require('node-fetch');
+const PORT = 8081
+const redirect_uri='http://localhost:8081/callback'
+const spotify_auth_uri = 'https://accounts.spotify.com/authorize?';
 
 app.use(cors())
-app.use(cookieSession({
-  name: 'sotify-auth-session',
-  keys: ['key1', 'key2']
-}))
+    .use(express.json())
+    .use(express.urlencoded({extended:false}))
 
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.get('/auth/spotify', passport.authenticate('spotify'), function(req, res) {
-    // The request will be redirected to spotify for authentication, so this
-    // function will not be called.
-  });
-app.get('/',isLoggedIn, (req, res) =>
-  res.send(`Hello ${req.user.displayName}`)
+app.get('/', (request, response)=>
+    response.send('Hello')
 )
-app.get(
-  '/auth/spotify',
-    passport.authenticate('spotify', {
-    scope: ['user-read-email', 'user-read-private'],
-    showDialog: true
-  }),
-  function(req, res) {
-    // The request will be redirected to spotify for authentication, so this
-    // function will not be called.
-  }
-);
+app.get('/login',(request,response)=>{
+    const scope = 'user-read-private user-read-private';
+    response.redirect(spotify_auth_uri+
+        querystring.stringify({
+            client_id: process.env.CLIENT_ID,
+            response_type: 'code',
+            redirect_uri: redirect_uri,
+            scope:scope,
+        }))
 
-  app.get(
-    '/auth/spotify/callback',
-    passport.authenticate('spotify', { failureRedirect: '/login' }),
-    function(req, res) {
-      // Successful authentication, redirect home.
-      res.redirect('http://localhost:3000');
-    }
-  );
-  app.get('/logout', (req,res)=>{
-    req.session=null;
-    req.logout();
-    res.redirect('/');
-  })
 
-app.listen(PORT, () => console.log(  `listening on port ${PORT}`))
+
+});
+app.get('/callback', (request, response)=>{
+    getAuth(request.query.code).then(response => console.log(response)).catch(err => console.log(err.message))
+})
+ function getAuth(auth_code) {
+   return fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic' + new Buffer.from(process.env.CLIENT_ID + ':' + process.env.CLIENT_SECRET).toString('base64'),
+        },
+        body:{
+            "grant_type": "authorization_code",
+            "code": auth_code,
+            "redirect_uri": redirect_uri,
+        }
+    })
+}
+
+
+app.listen(PORT, ()=>console.log(`listening on ${PORT}`));
